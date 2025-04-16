1d245301cc16aa4c9cb2667a6384e11454aa4d4a
RaihanDaffa04 <raihandaffaizzuddin77@gmail.com>
2025-04-16 10:23:18 +0800

    commit awal

---
+++ src/Bank.java
@@ -0,0 +1,24 @@
+public class Bank {
+    String noRekening;
+    String nama;
+    String namaIbu; 
+    String noHp;
+    String email;
+
+    public Bank(String noRekening, String nama, String namaIbu, String noHp, String email) {
+        this.noRekening = noRekening;
+        this.nama = nama;
+        this.namaIbu = namaIbu;
+        this.noHp = noHp;
+        this.email = email;
+    }
+
+    public void tampil() {
+        System.out.println("No Rekening: " + noRekening);
+        System.out.println("Nama: " + nama);
+        System.out.println("Nama Ibu: " + namaIbu);
+        System.out.println("No Hp: " + noHp);
+        System.out.println("Email: " + email);
+        System.out.println("---------------------------");
+    }
+}
