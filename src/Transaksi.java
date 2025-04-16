902e64e9cbfd4749307cac86efcf925c6c707aa0
Rayhwcakep <rayhanindra290805@gmail.com>
2025-04-16 09:44:34 +0700

    commit transaksi

--- src/Transaksi.java
+++ src/Transaksi.java
@@ -1,3 +1,31 @@
 public class Transaksi {
+        String kodeTransaksi;
+        double saldo;
+        double inOutSaldo; 
+        double finalSaldo;
+        String tanggalTransaksi;
+        String type;
+        Bank bankAcc;
     
-}
+        public Transaksi(String kodeTransaksi, double saldo, double inOutSaldo, double finalSaldo, String tanggalTransaksi, String type, Bank bankAcc ) {
+            this.kodeTransaksi = kodeTransaksi;
+            this.saldo = saldo;
+            this.inOutSaldo = inOutSaldo;
+            this.finalSaldo = finalSaldo;
+            this.tanggalTransaksi = tanggalTransaksi;
+            this.type = type;
+            this.bankAcc = bankAcc;
+        }
+    
+        public void tampil() {
+            System.out.println("kode transaksi: " + kodeTransaksi );
+            System.out.println("saldo: " + saldo);
+            System.out.println("in out saldo: " + inOutSaldo);
+            System.out.println("final saldo: " + finalSaldo);
+            System.out.println("tanggal transaksi: " + tanggalTransaksi);
+            System.out.println("type: " + type);
+            System.out.println("");
+            System.out.println("---------------------------");
+        }
+    }
+    
